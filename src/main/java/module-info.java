81d5623ef3f68cc2ce6b755249ba3c16256e7bbf
Sebastian Strobl <Sebastian@Strobl.net>
2019-07-18 21:43:12 +0200

    configured gradle and  JavaFX Hello World

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module datacrawler.main{
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens net.strobl;
+    opens net.strobl.management;
+}
---
+++ src/main/java/net/strobl/Launcher.java
@@ -0,0 +1,9 @@
+package net.strobl;
+
+import net.strobl.management.App;
+
+public class Launcher {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
