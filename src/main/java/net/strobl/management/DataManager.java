386d0fd28c9589d10a4f7570e6a61ecbb6752235
Sebastian Strobl <Sebastian@Strobl.net>
2019-07-24 17:59:28 +0200

    added fetch method for bills for testing with my own database

--- src/main/java/net/strobl/management/DataManager.java
+++ src/main/java/net/strobl/management/DataManager.java
@@ -2,12 +2,14 @@ package net.strobl.management;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DataManager {
     private String databaseType;
     private String url, username, password, table;
     private String jdbcDriver;
     private Connection connection;
+    private ArrayList<Bill> bills = new ArrayList<>();
     private ArrayList<String> columnNames = new ArrayList<>();
     private ArrayList<String> tableNames = new ArrayList<>();
     private boolean connected;
@@ -66,6 +68,39 @@ public class DataManager {
         }
     }
 
+    public void readAllBills() {
+        try {
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM " + table);
+            while (rs.next()) {
+                int billID = rs.getInt("id");
+                String project = rs.getString("projectname");
+                int amountInCent = rs.getInt("amountincent");
+                boolean isIntake = rs.getBoolean("isintake");
+                boolean isDigital = rs.getBoolean("isdigital");
+                boolean isPaid = rs.getBoolean("ispaid");
+                String dateOfOrder = rs.getString("dateorder");
+                String dateOfReceive = rs.getString("datereceived");
+                String dateOfPayment = rs.getString("datepayment");
+                String orderedBy = rs.getString("orderedby");
+                String seller = rs.getString("seller");
+                Array sqlItemArray = rs.getArray("items");
+                String[] itemArray = (String[]) sqlItemArray.getArray();
+                ArrayList<String> observableItems = new ArrayList<>(Arrays.asList(itemArray));
+                String reason = rs.getString("reason");
+
+                bills.add(new Bill(billID, project, amountInCent, isIntake, isDigital, isPaid, dateOfOrder, dateOfReceive, dateOfPayment, reason, orderedBy, seller, observableItems));
+            }
+            rs.close();
+            statement.close();
+        } catch (Exception e) {
+            System.out.println("Could not fetch bills");
+            //System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+    }
+
     public void readColumnNames() {
         try {
             DatabaseMetaData metaData = connection.getMetaData();
@@ -119,11 +154,15 @@ public class DataManager {
         return connected;
     }
 
-    public ArrayList<String> getTableNames(){
+    public ArrayList<Bill> getBills(){
+        return bills;
+    }
+
+    public ArrayList<String> getTableNames() {
         return tableNames;
     }
 
-    public ArrayList<String> getColumnNames(){
+    public ArrayList<String> getColumnNames() {
         return columnNames;
     }
 }
